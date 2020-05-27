afa8e5107687919f8eaf46ccce97a122b5fa8bdd
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2020-05-27 15:29:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-"use strict"
-btn.addEventListener('click', m_open)
+"use strict";
+btn.addEventListener('click', m_open);
 function m_open() {
     menu.classList.toggle('menu_is-open');
+}
+
+tema.addEventListener('click', Color);
+function Color() {
+    b.classList.toggle('chenge_color');
 }
