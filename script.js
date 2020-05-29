aeda4f5f6c674220ce2fc1b2eb034df19f31fe9c
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2020-05-29 09:14:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 "use strict";
+
 btn.addEventListener('click', m_open);
 function m_open() {
     menu.classList.toggle('menu_is-open');
 }
-
+var f = true
 tema.addEventListener('click', Color);
 function Color() {
+    if (f) {
+        tema.innerHTML = "Перейти на Тёмную сторону"
+        f = false
+    }
+    else {
+        tema.innerHTML = "Перейти на Светлую сторону"
+        f = true
+    }
     b.classList.toggle('chenge_color');
+    tema.classList.toggle('ch_color');
 }
